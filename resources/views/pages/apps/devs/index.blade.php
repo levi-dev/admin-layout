48a7863c9643a9b5fd3590b6498726b14494cfa9
tienlv <tienlv@smartosc.com>
2022-12-19 17:59:36 +0700

    update

--- resources/views/pages/apps/devs/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-admin-base-layout>
-
-    {{ theme()->getView('pages/apps/devs/_questions') }}
-
-</x-admin-base-layout>
--- resources/views/pages/documentation/general/datatables/overview/overview.blade.php
+++
@@ -1,7 +0,0 @@
-<x-admin-base-layout>
-
-    {{ theme()->getView('pages/documentation/general/datatables/overview/_overview') }}
-
-    {{ theme()->getView('pages/documentation/general/datatables/overview/_usage') }}
-
-</x-admin-base-layout>
